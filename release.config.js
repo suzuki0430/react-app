4458daf2febc0fe4dcd9b1fb2546e7fdbe9846dc
Atsushi <shimarisu.araiguma@gmail.com>
2021-03-28 22:35:31 +0900

    Add Create a Release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com /suzuki0430/react-app",
+  repositoryUrl: "https://github.com/suzuki0430/react-app",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
